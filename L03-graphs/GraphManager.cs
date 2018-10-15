e7e8c70ad85781f38aa534e2a5248723425c9707
timdhoffmann <9947384+timdhoffmann@users.noreply.github.com>
2018-10-15 10:26:33 +0200

    Added solution for Session03 Exercise "Graphs".

---
+++ L03-graphs/GraphManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace L03_graphs
+{
+    public class GraphManager
+    {
+        public List<Node> Nodes { get; private set; } = new List<Node>();
+        public List<Edge> Edges { get; private set; } = new List<Edge>();
+
+        //public void AddNode(string name, int position)
+        //{
+        //    Nodes.Add(new Node(name, position));
+        //}
+
+        public void AddEdge(Node nodeA, Node nodeB)
+        {
+            Edges.Add(new Edge(nodeA, nodeB));
+        }
+
+        public string OutputSet()
+        {
+            string output = "{";
+
+            foreach (var edge in Edges)
+            {
+                output += "({" + edge.Nodes[0].Name + "," + edge.Nodes[1].Name + "}" + ", " + edge.Weight + ") ,";
+            }
+
+            output += "}";
+
+            return output;
+        }
+    }
+}
---
+++ L03-graphs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L03_graphs
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var graphManager = new GraphManager();
+
+            var firstNode = new Node("firstNode", 0);
+            var secondNode = new Node("secondNode", 1);
+
+            graphManager.AddEdge(firstNode, secondNode);
+
+            Console.WriteLine(graphManager.OutputSet());
+
+            Console.ReadKey();
+        }
+    }
+}
