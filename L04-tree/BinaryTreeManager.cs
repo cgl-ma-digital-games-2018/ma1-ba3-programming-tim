530cf35c52f143a3fa17634eb0bbde29f5829926
timdhoffmann <9947384+timdhoffmann@users.noreply.github.com>
2018-10-24 20:12:49 +0200

    Starting solution for L04-Trees

---
+++ L04-tree/BinaryTreeManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L04_tree
+{
+    // Class for ordered binary trees
+    internal class BinaryTreeManager
+    {
+        private int mySum = 0;
+
+        // Output the tree in all three discussed orders
+        private void OutputTree()
+        {
+            // Execute something else
+            mySum = AddValues(1, 2);
+        }
+
+        private int AddValues(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
