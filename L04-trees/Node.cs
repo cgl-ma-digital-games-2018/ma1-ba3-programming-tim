c4e72a83a39c3535c0f5d5ef6cc0b033d9badaf4
timdhoffmann <9947384+timdhoffmann@users.noreply.github.com>
2018-10-25 09:14:40 +0200

    Added Node.cs and NodeList.cs

---
+++ L04-trees/Node.cs
@@ -0,0 +1,18 @@
+﻿namespace L04_trees
+{
+    internal class Node<T>
+    {
+        public NodeList<T> NodeList { get; }
+        public T Data { get; }
+
+        public Node(T data)
+        {
+            Data = data;
+        }
+
+        public Node(T data, NodeList<T> nodeList) : this(data)
+        {
+            NodeList = nodeList;
+        }
+    }
+}
---
+++ L04-trees/NodeList.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.ObjectModel;
+
+namespace L04_trees
+{
+    internal class NodeList<T> : Collection<Node<T>>
+    {
+    }
+}
