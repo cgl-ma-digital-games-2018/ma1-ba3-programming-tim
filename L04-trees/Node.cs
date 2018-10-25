9fcc39de52de1548c35c23544e1a1140765c7ae8
timdhoffmann <9947384+timdhoffmann@users.noreply.github.com>
2018-10-25 16:53:46 +0200

    Starting to implement BinaryTreeNode<T> class.

--- L04-trees/Node.cs
+++ L04-trees/Node.cs
@@ -1,9 +1,22 @@
-﻿namespace L04_trees
+﻿using System;
+
+namespace L04_trees
 {
+    internal class BinaryTreeNode<T> : Node<T>
+    {
+        public BinaryTreeNode(T node)
+        {
+        }
+    }
+
     internal class Node<T>
     {
         public NodeList<T> NodeList { get; }
         public T Data { get; }
+
+        public Node()
+        {
+        }
 
         public Node(T data)
         {
