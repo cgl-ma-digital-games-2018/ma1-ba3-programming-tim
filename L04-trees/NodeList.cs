8c2fdc6794468777ac4767cc4af901390bc326da
timdhoffmann <9947384+timdhoffmann@users.noreply.github.com>
2018-10-25 16:45:55 +0200

    NodeList implemented.

--- L04-trees/NodeList.cs
+++ L04-trees/NodeList.cs
@@ -4,5 +4,30 @@ namespace L04_trees
 {
     internal class NodeList<T> : Collection<Node<T>>
     {
+        public NodeList() : base()
+        {
+        }
+
+        public NodeList(int initialSize)
+        {
+            for (int i = 0; i < initialSize; i++)
+            {
+                base.Items.Add(default(Node<T>));
+            }
+        }
+
+        public Node<T> FindByValue(T value)
+        {
+            foreach (var node in Items)
+            {
+                // TODO: Comparison ok or need node.value.equals?
+                if (node.Equals(value))
+                {
+                    return node;
+                }
+            }
+
+            return null;
+        }
     }
 }
